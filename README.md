aac0320497bf4ea1bf95f9cbd0a44c2513bebd83
lhlyu <misakafs@gmail.com>
2023-06-25 18:56:00 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # dori
 ドリー
+
+
+## 参考
+
+- [flutter命令](https://docs.flutter.dev/reference/flutter-cli)
---
+++ android/app/src/main/kotlin/com/misakafs/dori/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.misakafs.dori
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
